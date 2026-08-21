63fa2addf617a4a10f0ce0e53575f6a9c7da6fcb
nanderson011 <nanderson011@student.butte.edu>
2026-08-20 18:43:22 -0700

    set up next.js project finished

---
+++ nanderson/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <a href="#" className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs hover:bg-neutral-secondary-medium">
+    <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">This will eventually be my website</h5>
+    <p className="text-body">I'm tired grandpa.</p>
+</a>
+  )
+}
